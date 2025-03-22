ddb268c2f0dd5492777fa55f797f238dad435936
hyperion9913 <supernoob7903@gmail.com>
2025-03-22 14:21:06 +0530

    fixed spheres

--- src/components/canvas/BallCanvas.jsx
+++ src/components/canvas/BallCanvas.jsx
@@ -1,4 +1,4 @@
-import React , { Suspense } from 'react'
+import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei'
 import CanvasLoader from '../Loader'
@@ -8,29 +8,24 @@ const Ball = (props) => {
 
   return (
     <Float speed={1} rotationIntensity={1} floatIntensity={1}>
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[0, 0, 1]} />
-      <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1,1]} />
-        <mesh
-        color="white"
-        polygonOffset
-        polygonOffsetFactor={-5}
-        flatShading
-        />
-        <Decal 
-        position={[0, 0, 1]}
-        rotation={[ 2 * Math.PI, 0, 6.25]}
-        map = {decal}
-        flatShading
-        />
-      </mesh>
-    </Float>
+  <ambientLight intensity={1} />
+  <directionalLight position={[0, 0, 1]} />
+  <mesh castShadow receiveShadow scale={2.5}>
+    <sphereGeometry args={[1, 32, 32]} /> 
+    <meshBasicMaterial color="white" />
+    <Decal 
+      position={[0, 0, 1]}
+      rotation={[2 * Math.PI, 0, 6.25]}
+      map={decal}
+      flatShading
+    />
+  </mesh>
+</Float>
   )
 }
 
-const BallCanvas = ({icon}) => {
-  return(
+const BallCanvas = ({ icon }) => {
+  return (
     <Canvas
       frameloop='demand'
       gl={{ preserveDrawingBuffer: true }}
