38a8a694422a7eccffad185a964cb5d2526b161c
Hyperion147 <supernoob7903@gmail.com>
2025-06-24 16:52:35 +0530

    updated project links

--- src/components/utils/HighlightedProject.jsx
+++ src/components/utils/HighlightedProject.jsx
@@ -113,7 +113,7 @@ const HighlightedProject = ({ isMobile }) => {
         <div className="flex flex-col sm:flex-row gap-3 w-full justify-center items-center mb-4">
           <a
             className="relative inline-flex h-10 overflow-hidden rounded-full p-[1px] focus:outline-none w-full sm:w-40 transition-all duration-300 hover:-translate-y-1 mb-2 sm:mb-0"
-            href="https://todo-hyper-neon.vercel.app/"
+            href="https://todo.suryansu.pro/todo"
             target="_blank"
           >
             <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#00AABA_0%,#0093A0_50%,#00AABA_100%)]" />
