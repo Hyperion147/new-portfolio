83e36c6825eb1c59bdb4c6105dbaa686c3d9682e
Hyperion147 <supernoob7903@gmail.com>
2025-09-19 13:19:40 +0530

    changes description in projectsection

--- src/constants/projectInfo.js
+++ src/constants/projectInfo.js
@@ -3,9 +3,10 @@ export const projectInfo = [
         id: 1,
         title: "todo.suryansu.pro",
         heading: "Todo App",
-        description: "A feature-rich Todo application designed for seamless task management. Built with React and Tailwind CSS, it offers a sleek with support for multiple themes to suit user preferences. The app integrates Supabase for robust authentication and data storage. Enhanced with GSAP animations, the UI delivers smooth, engaging interactions, while React Hot Toast provides feedback for user actions. Key features include task prioritization, due dates, and descriptions, all wrapped in a minimalist, user-friendly design.",
+        description:
+            "A feature-rich Todo application designed for seamless task management. Built with React and Tailwind CSS, it offers a sleek with support for multiple themes to suit user preferences. The app integrates Supabase for robust authentication and data storage. Enhanced with GSAP animations, the UI delivers smooth, engaging interactions, while React Hot Toast provides feedback for user actions. Key features include task prioritization, due dates, and descriptions, all wrapped in a minimalist, user-friendly design.",
         href: "https://todo.suryansu.pro/",
-        code:"https://github.com/Hyperion147/todo-supabase",
+        code: "https://github.com/Hyperion147/todo-supabase",
         image: "/highlightedProject/main.png",
         preview: "/highlightedProject/main2.png",
         tags: [
@@ -31,9 +32,10 @@ export const projectInfo = [
         id: 2,
         title: "blog.suryansu.pro",
         heading: "Blog",
-        description: "This website is a personal blog built with React and Tailwind CSS, combining modern web development techniques with a clean, responsive design. It offers a seamless reading experience and interactive features to engage visitors. The blog showcases my ability to create intuitive user interfaces while sharing insights and ideas.",
+        description:
+            "This website is a personal blog built with React and Tailwind CSS, combining modern web development techniques with a clean, responsive design. It offers a seamless reading experience and interactive features to engage visitors. The blog showcases my ability to create intuitive user interfaces while sharing insights and ideas.",
         href: "https://blog.suryansu.pro/",
-        code:"https://github.com/Hyperion147/blog",
+        code: "https://github.com/Hyperion147/blog",
         image: "/projects/blog.png",
         preview: "/projects/blogpreview.png",
         tags: [
@@ -48,14 +50,15 @@ export const projectInfo = [
             {
                 id: 3,
                 name: "#gsap",
-            }
+            },
         ],
     },
     {
         id: 3,
         title: "panipatconnect.com",
         heading: "Green Panipat Mission",
-        description: "This website is a personal blog built with React and Tailwind CSS, combining modern web development techniques with a clean, responsive design. It offers a seamless reading experience and interactive features to engage visitors. The blog showcases my ability to create intuitive user interfaces while sharing insights and ideas.",
+        description:
+            "Panipat Connect is an innovative plantation website where users can add and geotag their plants, fostering a community of nature enthusiasts. The platform features a competitive leaderboard to encourage engagement and an admin dashboard for seamless management. Users can also access a dedicated plant section with downloadable certificates recognizing their contributions and milestones. This project showcases my skills in full-stack development, geolocation integration, and dynamic content generation.",
         href: "https://panipatconnect.com",
         image: "/projects/green.png",
         preview: "/projects/green.png",
@@ -75,7 +78,7 @@ export const projectInfo = [
             {
                 id: 4,
                 name: "#shadcn",
-            }
+            },
         ],
     },
-]
+];
